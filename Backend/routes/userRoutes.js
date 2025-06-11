d2956c384dfd3cae6ba1cb9897474dbd387c7308
rudresh21 <rudreshsankpal21@gmail.com>
2025-06-11 23:49:50 +0530

    User registration

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,17 +4,8 @@ const userRouter = express.Router();
 const jwt = require("jsonwebtoken");
 
 // User Registration
-userRouter.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
-
-  try {
-    const user = await User.create({
-      username,
-      email,
-      password,
-    });
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+userRouter.get("/", async (req, res) => {
+  res.send("User Registration");
 });
+
+module.exports = userRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const userRouter = require("./routes/userRoutes");
 const app = express();
+const PORT = process.env.PORT || 3000;
 require("dotenv").config();
+
+// routes
+app.use("/register", userRouter);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
