c2714a1530f960740f560de7aa8df301d406aa9c
rudresh21 <rudreshsankpal21@gmail.com>
2025-06-11 13:41:49 +0530

    Created User Model Schema

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -2,23 +2,27 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 // User Schema
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
       required: true,
       trim: true,
     },
+
     email: {
       type: String,
       required: true,
+      unique: true,
       trim: true,
       lowercase: true,
     },
+
     password: {
       type: String,
       required: true,
     },
+
     isAdmin: {
       type: Boolean,
       default: false,
@@ -29,7 +33,7 @@ const userSchema = mongoose.Schema(
   }
 );
 
-// Hash user password before saving
+// Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
@@ -37,8 +41,8 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-// Method to compare user password
-userSchema.methods.matchPassword = async function (password) {
+// Compare password
+userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
