5372c38f559146b447694e3d8b1ee05e75a190e4
rudresh21 <rudreshsankpal21@gmail.com>
2025-06-11 15:45:20 +0530

    Created groupModel Schema

---
+++ Backend/models/groupModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+// Group Schema
+const groupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Model
+const Group = mongoose.model("Group", groupSchema);
+
+module.exports = Group;
