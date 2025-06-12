08d82bdef1d9b4c27ae088eb99194e1d6b28d760
rudresh21 <rudreshsankpal21@gmail.com>
2025-06-12 20:56:01 +0530

    Added route for user to join a group

--- Backend/routes/groupRoutes.js
+++ Backend/routes/groupRoutes.js
@@ -24,7 +24,7 @@ groupRouter.post("/", protect, isAdmin, async (req, res) => {
 });
 
 // get all groups
-groupRouter.get("/", async (req, res) => {
+groupRouter.get("/", protect, async (req, res) => {
   try {
     const groups = await Group.find()
       .populate("admin", "username email")
@@ -35,4 +35,30 @@ groupRouter.get("/", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+// Join group
+groupRouter.post("/:groupId/join", protect, async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.groupId);
+
+    if (!group) {
+      res.status(404).json({ message: "Group not found" });
+    }
+
+    // Check if user is already a member of the group
+    if (group.members.includes(req.user._id)) {
+      res
+        .status(400)
+        .json({ message: "User is already a member of the group" });
+    }
+
+    // Add user to group
+    group.members.push(req.user._id);
+    await group.save();
+    res.status(200).json({ message: "User joined group successfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Failed to join group" });
+  }
+});
+
 module.exports = groupRouter;
