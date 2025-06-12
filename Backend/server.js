12d06be1fcbe166c3c86e9bc65a59e87749aacaa
rudresh21 <rudreshsankpal21@gmail.com>
2025-06-12 14:44:54 +0530

    Configured server

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,40 @@
+require("dotenv").config();
 const express = require("express");
 const userRouter = require("./routes/userRoutes");
-const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const http = require("http");
+const socket = require("socket.io");
+const socketIo = require("./socket.io");
 const PORT = process.env.PORT || 3000;
-require("dotenv").config();
 
-// routes
-app.use("/register", userRouter);
-// Start the server
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+const app = express();
+const server = http.createServer(app);
+const io = socket(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Initialize
+socketIo(io);
+
+// Routes
+app.use("/api/users", userRouter);
+
+// connect to db and start the server
+mongoose
+  .connect("mongo url")
+  .then(() => {
+    console.log("Connected to DB");
+    server.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log("Connection faied", err));
