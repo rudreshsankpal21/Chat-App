8b3392de86c9d0e1f25a64cebbc14e836f4660a2
rudresh21 <rudreshsankpal21@gmail.com>
2025-06-12 14:30:42 +0530

    Added user Login with token

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -27,4 +27,36 @@ userRouter.post("/register", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+// User Login route
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (user && (await user.comparePassword(password))) {
+      res.json({
+        user: {
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id),
+        },
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 module.exports = userRouter;
